1daa73aa683e5b7a3c19e6b83a0a0202bfffbcbb
vmorreel <dawin.vmorreel@gmail.com>
2017-04-03 11:09:18 +0200

    Add minify method to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,24 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var minify = require('gulp-minify');
+var cleanCss = require('gulp-clean-css');
 
 gulp.task('pack-js', function () {	
 	return gulp.src(['assets/js/main.js', 'assets/js/jquery-3.2.0.min.js'])
 	.pipe(concat('application.js'))
+	.pipe(minify({
+		ext:{
+			min:'.js'
+		},
+		noSource: true
+	}))
 	.pipe(gulp.dest('build/js'));
 });
 
 gulp.task('pack-css', function () {	
 	return gulp.src(['assets/css/main.css', 'assets/css/fonticon.css'])
 	.pipe(concat('stylesheet.css'))
+	.pipe(cleanCss())
 	.pipe(gulp.dest('build/css'));
 });
 
