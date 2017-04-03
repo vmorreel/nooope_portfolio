37bcd106049be9088557d3a030375ab7629eb3f2
vmorreel <dawin.vmorreel@gmail.com>
2017-04-03 11:36:56 +0200

    Add Process to cache css \& js files

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var minify = require('gulp-minify');
 var cleanCss = require('gulp-clean-css');
+var rev = require('gulp-rev');
 
 gulp.task('pack-js', function () {	
 	return gulp.src(['assets/js/jquery-3.2.0.min.js', 'assets/js/main.js'])
@@ -12,14 +13,24 @@ gulp.task('pack-js', function () {
 		},
 		noSource: true
 	}))
-	.pipe(gulp.dest('build/js'));
+	.pipe(rev())
+	.pipe(gulp.dest('build/js'))
+	.pipe(rev.manifest('build/rev-manifest.json', {
+		merge: true
+	}))
+	.pipe(gulp.dest(''));
 });
 
 gulp.task('pack-css', function () {	
 	return gulp.src(['assets/css/fonticon.css', 'assets/css/main.css'])
 	.pipe(concat('stylesheet.css'))
 	.pipe(cleanCss())
-	.pipe(gulp.dest('build/css'));
+	.pipe(rev())
+	.pipe(gulp.dest('build/css'))
+	.pipe(rev.manifest('build/rev-manifest.json', {
+		merge: true
+	}))
+	.pipe(gulp.dest(''));
 });
 
 gulp.task('default', ['pack-js', 'pack-css']);
