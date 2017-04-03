6dc640859d6f296c6905e9d3e353af5524a57ae6
vmorreel <dawin.vmorreel@gmail.com>
2017-04-03 11:05:54 +0200

    Add minify dependencies to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ var concat = require('gulp-concat');
 gulp.task('pack-js', function () {	
 	return gulp.src(['assets/js/main.js', 'assets/js/jquery-3.2.0.min.js'])
 	.pipe(concat('application.js'))
-	.pipe(gulp.dest('public/build/js'));
+	.pipe(gulp.dest('build/js'));
 });
 
 gulp.task('pack-css', function () {	
 	return gulp.src(['assets/css/main.css', 'assets/css/fonticon.css'])
 	.pipe(concat('stylesheet.css'))
-	.pipe(gulp.dest('public/build/css'));
+	.pipe(gulp.dest('build/css'));
 });
 
 gulp.task('default', ['pack-js', 'pack-css']);
